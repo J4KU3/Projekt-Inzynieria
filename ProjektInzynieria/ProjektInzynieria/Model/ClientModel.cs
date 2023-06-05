952a06e8856fdf0d3b66e2605d61271dbdea4b34
Jakub <kuba24481@wp.pl>
2023-06-05 16:37:35 +0200

    DataBaseConnection and Create all Models

---
+++ ProjektInzynieria/ProjektInzynieria/Model/ClientModel.cs
@@ -0,0 +1,13 @@
+﻿using ProjektInzynieria.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektInzynieria.Model
+{
+  public class ClientModel: Clients
+    {
+    }
+}
---
+++ ProjektInzynieria/ProjektInzynieria/Model/EmployeesModel.cs
@@ -0,0 +1,24 @@
+﻿using ProjektInzynieria.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ProjektInzynieria.Model
+{
+    public class EmployeesModel:Employees
+    {
+        public EmployeesModel(Employees Copyemployees)
+        {
+            employeeID = Copyemployees.employeeID;
+            EFirstName = Copyemployees.EFirstName;
+            ELastName = Copyemployees.ELastName;
+            Phone = Copyemployees.Phone;
+            Mail = Copyemployees.Mail;
+            Passsword = Copyemployees.Passsword;
+            IsAdmin = Copyemployees.IsAdmin;
+        }
+    }
+}
